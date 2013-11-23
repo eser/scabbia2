874b2df064ce63fcaf3eb42298e5d36d1b668fab
larukedi <eser@sent.com>
2013-11-23 20:01:08 +0200

    * some modifications on applications.yml.

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+
+if (!file_exists($scabbiaLoader = __DIR__ . '/vendor/autoload.php')) {
+    throw new RuntimeException('Unable to load Composer which is required for Scabbia2. Run `php bin/scabbia upgrade`.');
+}
+
+$loader = require($scabbiaLoader);
