440116a9a844f79dbd73e6160bc79376e0330880
Eser Ozvataf <eser@sent.com>
2014-09-16 20:45:29 +0300

    * codepools. * updated composer.json definitions.

---
+++ src/core/MyProject/FrontModule/Controllers/BaseController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Scabbia2 PHP Framework Skeleton Project
+ * http://www.scabbiafw.com/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @link        http://github.com/scabbiafw/scabbia2-skel for the canonical source repository
+ * @copyright   2010-2013 Scabbia Framework Organization. (http://www.scabbiafw.com/)
+ * @license     http://www.apache.org/licenses/LICENSE-2.0 - Apache License, Version 2.0
+ */
+
+namespace MyProject\FrontModule\Controllers;
+
+use Scabbia\Mvc\Controller;
+
+/**
+ * BaseController controller base
+ *
+ * @package     MyProject\FrontModule\Controllers
+ * @author      Eser Ozvataf <eser@sent.com>
+ * @since       2.0.0
+ */
+abstract class BaseController extends Controller
+{
+}
