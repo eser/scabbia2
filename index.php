c9d6145adffcaac48eb9833f893c3cce6d2d48a2
Eser Ozvataf <eser@sent.com>
2014-10-06 03:30:39 +0300

    * docblocks updated. * skeleton updated to adapt loader changes.

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
 /**
- * Scabbia2 PHP Framework Skeleton Project
+ * Scabbia2 Standard Package
  * http://www.scabbiafw.com/
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @link        http://github.com/scabbiafw/scabbia2-skel for the canonical source repository
- * @copyright   2010-2013 Scabbia Framework Organization. (http://www.scabbiafw.com/)
+ * @link        http://github.com/scabbiafw/scabbia2 for the canonical source repository
+ * @copyright   2010-2014 Scabbia Framework Organization. (http://www.scabbiafw.com/)
  * @license     http://www.apache.org/licenses/LICENSE-2.0 - Apache License, Version 2.0
  */
 
 // MD # autoloader initialization
-// MD - determine vendor path
-$tVendorPath = __DIR__ . "/vendor";
+// MD - determine the paths
+$tBasePath = __DIR__;
 
 // MD - instantiate and register the loader
-require "{$tVendorPath}/scabbiafw/scabbia2-fw/src/Loaders/Loader.php";
-$tLoader = \Scabbia\Loaders\Loader::init("{$tVendorPath}/composer");
+require "{$tBasePath}/vendor/scabbiafw/scabbia2-fw/src/Loaders/Loader.php";
+$tLoader = \Scabbia\Loaders\Loader::init($tBasePath);
 
 // MD # framework initialization
 use Scabbia\Framework\Core;
